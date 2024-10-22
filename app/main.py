bab853b3b4871281db25c93a01c9db214567f99f
syahrilramadhan775 <syahrilramadhan775@gmail.com>
2024-10-22 10:20:54 +0700

    update: main and environment variable

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from databases.Collection import lifespan
 from routes.v1.api import endpoints
+import uvicorn
+from dotenv import dotenv_values
 
+env = dotenv_values("../.env")   
 app = FastAPI(lifespan=lifespan)
-
 app.include_router(endpoints, prefix='/api/v1')
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host=env["HOST"], port=int(env["PORT"]), reload=env["RELOAD"])
