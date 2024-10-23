fe9a59dd49d5c39e34f470fd233a80a9480c418b
syahrilramadhan775 <syahrilramadhan775@gmail.com>
2024-10-23 16:54:34 +0700

    refactoring

--- app/seeders.py
+++ app/seeders.py
@@ -7,15 +7,8 @@ env = dotenv_values("../.env")
 mongoClient = MongoClient(env["MONGO_CONNECTION_STRING"])
 db = mongoClient.get_database(f'{env['DB_NAME']}')
 userCollection = db.get_collection('jsonModel_100')
-
-# print(userCollection)
-
-    # async def createUser(request: None, body: userCreate):
-    #     collection= await Users.__collection__()
-    #     user= collection.insert_one({'username': body.username, 'name': body.name})
-    #     data= collection.find_one({"_id": user.inserted_id})
-    #     return { "status": status.HTTP_201_CREATED, "data": serializeUser(data) }
 data= pd.read_csv('test.csv')
+
 for index, row in data.iterrows():
     userCollection.insert_one({'title': row['name'], 'content_json': row['username'], 'description_json': row['description_json']})
     print(row['name'])
