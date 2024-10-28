c5f51551a8aca11bee30d7cc22dd0d9b2b34b5aa
syahrilramadhan775 <syahrilramadhan775@gmail.com>
2024-10-28 13:45:24 +0700

    PGSQL integration

--- app/schemas/assets.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class AssetBase(BaseModel):
-    title: str
-    asset_url: str
-
-class AssetPath(BaseModel):
-    asset_url: str
--- app/serialization/user.py
+++
@@ -1,9 +0,0 @@
-def serializeUser(data) -> dict:
-    return {
-        "id": str(data['_id']),
-        "username": str(data['username']),
-        "name": str(data['name']),
-    }
-
-def serializeUsers(data) -> list:
-    return [serializeUser(data=data) for data in data]
