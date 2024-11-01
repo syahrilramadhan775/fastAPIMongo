fe2bf8848f8560eae3c121dcd17965fbdf33fe2f
syahrilramadhan775 <syahrilramadhan775@gmail.com>
2024-11-01 14:12:36 +0700

    comment

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
+# Get Library
 from routes.api import endpoints
 from dotenv import dotenv_values
 from fastapi import FastAPI
 import uvicorn
 
+# create variable 
 env = dotenv_values("../.env")
-
 app = FastAPI()
 app.include_router(endpoints, prefix='/api/v1')
 
+# conditional input process name = main.py
 if __name__ == '__main__':
     uvicorn.run("main:app", host=env["HOST"], port=int(env["PORT"]), reload=env["RELOAD"])
